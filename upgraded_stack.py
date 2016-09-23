0e48435cdfd72e634a8ab31e8c6b7267e9dac7b0
naormauda <naormauda100@gmail.com>
2016-09-23 11:32:22 +0300

    work
    work

--- upgraded_stack.py
+++ upgraded_stack.py
@@ -8,11 +8,19 @@ class UpgradedStack(Stack):
         self.__max_stack = Stack()
 
     def max_val(self):
-        # return self.__max_stack.peek()
-        pass
+        return self.__max_stack.peek()
 
     def push(self, item):
-        pass
+        if self.__max_stack.is_empty() or item >= self.__max_stack.peek():
+            self.__max_stack.push(item)
+            super(UpgradedStack,self).push(item)
+
+        else:
+            super(UpgradedStack,self).push(item)
 
     def pop(self):
-        pass
+        if super(UpgradedStack,self).peek() == self.__max_stack.peek():
+            self.__max_stack.pop()
+            return super(UpgradedStack,self).pop()
+        else:
+            return super(UpgradedStack,self).pop()
